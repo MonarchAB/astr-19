402e3a845ab643fbad8752731cb7656816031053
Audun Burton <audun.burton@gmail.com>
2024-10-20 20:05:51 -0700

    Add files via upload

---
+++ ASTR-19 Session 2/main.py
@@ -0,0 +1,14 @@
+float1 = 1.23
+float2 = 6.78
+sumOfFloats = float1 + float2
+
+int1 = 49
+int2 = 37
+differenceOfInts = int1 - int2
+
+productOfIntAndFloat = int1 * float1
+
+
+print("The sum of the floats is: " + str(sumOfFloats) + " " + str(type(sumOfFloats)))
+print("The difference of the integers is: " + str(differenceOfInts) + " " + str(type(differenceOfInts)))
+print("The product of the integer and float is: " + str(productOfIntAndFloat) + " " + str(type(productOfIntAndFloat)))
