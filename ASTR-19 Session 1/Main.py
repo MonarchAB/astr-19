32da55ef12c7b173bef5d3da2e88c21a0668aa25
Audun Burton <audun.burton@gmail.com>
2024-10-20 19:54:16 -0700

    Add files via upload

---
+++ ASTR-19 Session 1/Main.py
@@ -0,0 +1,11 @@
+myName = "Audun Altair McGuire Burton"
+myPronouns = "(He/Him)"
+myMovie = "My favorite movie is Mad Max: Fury Road, in part due to the terrific practical effects used to make the movie."
+myFood = "If I had to pick a favorite food, it would be spaghetti bolognese, as it's a meal my father makes especially well."
+
+
+
+print("My Full Name Is: " + myName)
+print("My Pronouns Are: " + myPronouns)
+print(myMovie)
+print(myFood)
